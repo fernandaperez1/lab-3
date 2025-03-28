5cbaf0bf95d4a5eb203f5e409141057efe5783a3
Fernanda Pérez <faperez1@miuandes.cl>
2025-03-28 12:02:21 -0300

    subiendo todo

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+    def home
+    end
+  
+    def about
+    end
+  
+    def contact
+    end
+  
+    def submit_contact
+      flash[:notice] = "Thank you for your message, we will contact you soon."
+      redirect_to contact_path
+    end
+  end
+  
